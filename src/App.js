b3a02889947051403dafa7f537cd5714635817c9
Ethan Rasmussen <ethanmr3@illinois.edu>
2025-05-24 19:39:54 -0500

    Handle keyboard inputs

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import { Ground } from './components/ground';
+import { Player } from './components/player';
 
 function App() {
   return (
     <>
-      <div>BlockgameJS: A Minecraft-Inspired Block Builder</div>
+      {/* <div>BlockgameJS: A Minecraft-Inspired Block Builder</div> */}
       <Canvas>
         <Sky sunPosition={[100,100,20]}/>
         <ambientLight intensity={1.2}/>
         <Physics>
+          <Player />
           <Ground />
         </Physics>
       </Canvas>
